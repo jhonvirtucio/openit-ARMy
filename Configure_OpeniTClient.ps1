cd1112309ef5d3da1310d8d67f93c809a1294ad6
Jhon Angelo C. Virtucio <jvirtucio@openit.com>
2020-07-07 14:35:16 +0800

    Add installation log to script

--- Configure_OpeniTClient.ps1
+++ Configure_OpeniTClient.ps1
@@ -1,2 +1,3 @@
-New-Item -Path '%ProgramData%\\Openit' -ItemType Directory 
-Start-Process msiexec.exe -Wait -ArgumentList "/I https://privatebox.openit.com/67880d02f530b30df656b7f2226ed204/openit_client_windows_x64.msi /l*v %ProgramData%\\Openit\\openit_install.log /quiet"
+$OpenitProgramData = $Env:ProgramData + '\\Openit'
+if ( !( Test-Path -Path $OpenitProgramData -PathType Container ) ) { New-Item -Path $OpenitProgramData -ItemType Directory }
+Start-Process msiexec.exe -Wait -ArgumentList "/I https://privatebox.openit.com/67880d02f530b30df656b7f2226ed204/openit_client_windows_x64.msi /l*v $OpenitProgramData\\openit_install.log /quiet"

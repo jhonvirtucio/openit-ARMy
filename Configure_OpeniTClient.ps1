06f4bf1d8a45f6696ee4da66ddafca1d61ce6d5a
Jhon Angelo C. Virtucio <jvirtucio@openit.com>
2020-07-08 18:02:16 +0800

    Parameterized InstallerURL

--- Configure_OpeniTClient.ps1
+++ Configure_OpeniTClient.ps1
@@ -1,6 +1,7 @@
 Param(
     [string] [Parameter(Mandatory = $true)] $SERVERURI
+    [string] [Parameter(Mandatory = $true)] $INSTALLERURL
 )
 $OpenitProgramData = $Env:ProgramData + "\Openit"
 if ( !( Test-Path -Path $OpenitProgramData -PathType Container ) ) { New-Item -Path $OpenitProgramData -ItemType Directory }
-Start-Process msiexec.exe -Wait -ArgumentList "/I https://storageopenit.blob.core.windows.net/clientinstallers/openit_9_6_30_client_windows_x64.msi SERVERURI=$SERVERURI /l*v $OpenitProgramData\\openit_install.log /quiet"
+Start-Process msiexec.exe -Wait -ArgumentList "/I $INSTALLERURL SERVERURI=$SERVERURI /l*v $OpenitProgramData\\openit_install.log /quiet"

9b8e8d197e27aeb80ea6b41ec81e1bf619d5b77f
Jhon Angelo C. Virtucio <jvirtucio@openit.com>
2020-07-09 00:17:35 +0800

    Add two way on deploying openit client

--- Configure_OpeniTClient.ps1
+++ Configure_OpeniTClient.ps1
@@ -1,7 +1,18 @@
 Param(
     [string] [Parameter(Mandatory = $true)] $SERVERURI,
-    [string] [Parameter(Mandatory = $true)] $INSTALLERURL
+    [string] [Parameter(Mandatory = $true)] $INSTALLERURL,
+    [switch] $privateBoxSource
 )
-$OpenitProgramData = $Env:ProgramData + "\Openit"
+$OpenitProgramData = "$Env:ProgramData\Openit"
 if ( !( Test-Path -Path $OpenitProgramData -PathType Container ) ) { New-Item -Path $OpenitProgramData -ItemType Directory }
-Start-Process msiexec.exe -Wait -ArgumentList "/I $INSTALLERURL SERVERURI=$SERVERURI /l*v $OpenitProgramData\\openit_install.log /quiet"
+
+if ($privateBoxSource) {
+    # Install openit client directly from the privatebox
+    Start-Process msiexec.exe -Wait -ArgumentList "/I $INSTALLERURL SERVERURI=$SERVERURI /l*v $OpenitProgramData\\openit_install.log /quiet"
+}
+else {
+    # Install openit client from the azure blob storage
+    $installerPath = "$OpenitProgramData\openit_client_installer.msi"
+    (New-Object System.Net.WebClient).DownloadFile($INSTALLERURL, $installerPath)
+    Start-Process msiexec.exe -Wait -ArgumentList "/I $installerPath SERVERURI=$SERVERURI /l*v $OpenitProgramData\openit_install.log /quiet
+}
